4e53cbaa5590f43e2015d2aeb99a61f808dfe9b7
prajwal <prajwal@clevertap.com>
2024-10-22 07:00:43 +0530

    Android : PE variable's synced

--- app/src/main/java/com/clevertap/demo/PEActivity.kt
+++ app/src/main/java/com/clevertap/demo/PEActivity.kt
@@ -7,8 +7,11 @@ import com.clevertap.android.sdk.CleverTapAPI
 import com.clevertap.android.sdk.variables.Var
 import com.clevertap.android.sdk.variables.callbacks.VariableCallback
 
+
 class PEActivity : AppCompatActivity() {
     private var cleverTap: CleverTapAPI? = null
+    val user = HashMap<String, Any>()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,31 +34,35 @@ class PEActivity : AppCompatActivity() {
 
 
 
-        val var1: Var<Byte> = cleverTap!!.defineVariable("var_byte", 1)
+//        val var1: Var<Byte> = cleverTap!!.defineVariable("var_byte", 1)
+//
+//        val var2: Var<Short> = cleverTap!!.defineVariable("var_short", 2)
+//
+//        val var3: Var<Int> = cleverTap!!.defineVariable("var_int", 3)
+//
+//        val var4: Var<Long> = cleverTap!!.defineVariable("var_long", 4L)
+//
+//        val var5: Var<Float> = cleverTap!!.defineVariable("var_float", 5F)
+//
+//        val var6: Var<Double> = cleverTap!!.defineVariable("var_double", 6.0)
+//
+//        val var7: Var<String> = cleverTap!!.defineVariable("var_string", "str")
+//
+//        val var8: Var<Boolean> = cleverTap!!.defineVariable("var_boolean", true)
 
-        val var2: Var<Short> = cleverTap!!.defineVariable("var_short", 2)
 
-        val var3: Var<Int> = cleverTap!!.defineVariable("var_int", 3)
-
-        val var4: Var<Long> = cleverTap!!.defineVariable("var_long", 4L)
-
-        val var5: Var<Float> = cleverTap!!.defineVariable("var_float", 5F)
-
-        val var6: Var<Double> = cleverTap!!.defineVariable("var_double", 6.0)
-
-        val var7: Var<String> = cleverTap!!.defineVariable("var_string", "str")
-
-        val var8: Var<Boolean> = cleverTap!!.defineVariable("var_boolean", true)
+        user["userType"] = "Gold";
+        user["plan"] = "Yearly";
 
 
 // You must instruct CleverTap SDK that you have such definition by using:
         // You must instruct CleverTap SDK that you have such definition by using:
-        cleverTap!!.parseVariablesForClasses(MyClass::class.java)
+//        cleverTap!!.parseVariablesForClasses(MyClass::class.java)
 
 // You must instruct CleverTap SDK that you have such a definition by using:
         // You must instruct CleverTap SDK that you have such a definition by using:
-        val instance = MyClass()
-        cleverTap!!.parseVariables(instance)
+//        val instance = MyClass()
+        cleverTap!!.defineVariable("userInfo",user)
 
         cleverTap!!.syncVariables();
 
@@ -69,13 +76,7 @@ class PEActivity : AppCompatActivity() {
 
 
 
-        var3.addValueChangedCallback(object : VariableCallback<Int>() {
-            override fun onValueChanged(varInstance: Var<Int>) {
-                // invoked on app start and whenever value is changed
-                Log.d("ttt", "onCreate: onValueChanged =>${varInstance.stringValue}")
 
-            }
-        })
 
 
 
